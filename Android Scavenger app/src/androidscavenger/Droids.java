a74e9a2a4a15a2a22bb5e9dbc7ead6cbe1a04e81
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:16:32 +0100

    Update Droids.java

--- Android Scavenger app/src/androidscavenger/Droids.java
+++ Android Scavenger app/src/androidscavenger/Droids.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package androidscavenger;
 
-/**
- *
- * @author M4K3L
- */
+/*
+* Android Scavenger is an application where you can search through inventory which contains randomly generated 400 
+* new line of droids which were created by taking parts from old droid. This application is using very complex algorithm.
+*
+* @author Michal Switala
+* Copyright © 2021 belongs solely to Michal Switala. 
+* You can reach me out on : https://github.com/michaelPro89
+*/
 public abstract class Droids {
     
     // required parts for each Droid
